258d396ce20c76c9d7aae2ce3acfa6a5b4d1cd39
IBLINETUM\juan-pedro.sanchez <juan-pedro.sanchez@inetum.com>
2023-08-18 23:31:48 +0200

    Initial commit.

---
+++ src/main/java/com/misispiclix/elementalrealms/ElementalRealmsApplication.java
@@ -0,0 +1,13 @@
+package com.misispiclix.elementalrealms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementalRealmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementalRealmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misispiclix/elementalrealms/ElementalRealmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misispiclix.elementalrealms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementalRealmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
